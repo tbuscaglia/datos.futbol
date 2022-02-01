1df4afc119c286b9446384dde2da6b3f959b4931
tbuscaglia <tebuscaglia@gmail.com>
2022-02-01 19:24:05 -0300

    Se Filtro las columnas necesarias y se agrego un ranking de GD

--- Scripts/DiferenciaDeGol.R
+++ Scripts/DiferenciaDeGol.R
@@ -36,50 +36,74 @@ temp_7 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/18-19.csv")
 temp_8 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/19-20.csv")
 temp_9 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/20-21.csv")
 
-# total_temporadas <- c(
-#   '12.3', '13.14', '14.15', '15.16', '16.17', '17.18', '18.19', '19.20', '20.21'
-# )
-
-total_temporadas <- c(
-  temp_1, temp_2, temp_3, temp_4, temp_5, temp_6, temp_7, temp_8, temp_9
-)
-
 # 4 - Filtro Informacion --------------------------------------------------
 
-temp_1 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_2 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_3 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_4 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_5 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_6 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_7 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_8 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
-temp_9 %>% 
-  group_by(Squad) %>% 
-  select("Rk", "Squad", "GD")
+
+
+temp_1 <- temp_1 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad) 
+  
+temp_2 <- temp_2 %>% 
+    select("Rk", "Squad", "GD") %>% 
+    group_by(Squad)  
+  
+temp_3 <- temp_3 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad)  
+  
+temp_4 <- temp_4 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad)  
+  
+temp_5 <- temp_5 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad)  
+  
+temp_6 <- temp_6 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad)  
+  
+temp_7 <- temp_7 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad) 
+  
+temp_8 <- temp_8 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad) 
+  
+temp_9 <- temp_9 %>% 
+  select("Rk", "Squad", "GD") %>% 
+  group_by(Squad) 
+  
 
 # 5 - Crear Ranking Dif Gol -----------------------------------------------
 
-temp_1 %>% 
-  arrange(desc(GD)) 
+temp_1$rank <- NA
+temp_1$rank[order(-temp_1$GD)] <- 1:nrow(temp_1)
   
-  
+temp_2$rank <- NA
+temp_2$rank[order(-temp_2$GD)] <- 1:nrow(temp_2)
 
+temp_3$rank <- NA
+temp_3$rank[order(-temp_3$GD)] <- 1:nrow(temp_3)
+
+temp_4$rank <- NA
+temp_4$rank[order(-temp_4$GD)] <- 1:nrow(temp_4)
+
+temp_5$rank <- NA
+temp_5$rank[order(-temp_5$GD)] <- 1:nrow(temp_5)
+
+temp_6$rank <- NA
+temp_6$rank[order(-temp_6$GD)] <- 1:nrow(temp_6)
+
+temp_7$rank <- NA
+temp_7$rank[order(-temp_7$GD)] <- 1:nrow(temp_7)
+
+temp_8$rank <- NA
+temp_8$rank[order(-temp_8$GD)] <- 1:nrow(temp_8)
+
+temp_9$rank <- NA
+temp_9$rank[order(-temp_9$GD)] <- 1:nrow(temp_9)
 
 
