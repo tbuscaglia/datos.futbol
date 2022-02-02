c0212340a75437dbf09fa446ecf2ebfa92029c7a
tbuscaglia <tebuscaglia@gmail.com>
2022-02-02 15:20:33 -0300

    Optimizacion del codigo y creacion de diferencia a estudiar

--- Scripts/DiferenciaDeGol.R
+++ Scripts/DiferenciaDeGol.R
@@ -26,19 +26,20 @@ rm(list = c("paquetes"))
 
 # 3 - Traigo Informacion --------------------------------------------------
 
-temp_1 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/12-13.csv")
-temp_2 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/13-14.csv")
-temp_3 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/14-15.csv")
-temp_4 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/15-16.csv")
-temp_5 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/16-17.csv")
-temp_6 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/17-18.csv")
-temp_7 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/18-19.csv")
-temp_8 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/19-20.csv")
-temp_9 <- read.csv2("~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV/20-21.csv")
+path <- "~/R/Futbol.R/datos.futbol/Tablas Temporadas/CSV"
 
-# 4 - Filtro Informacion --------------------------------------------------
+temp_1 <- read.csv2(glue::glue("{path}/12-13.csv"))
+temp_2 <- read.csv2(glue::glue("{path}/13-14.csv"))
+temp_3 <- read.csv2(glue::glue("{path}/14-15.csv"))
+temp_4 <- read.csv2(glue::glue("{path}/15-16.csv"))
+temp_5 <- read.csv2(glue::glue("{path}/16-17.csv"))
+temp_6 <- read.csv2(glue::glue("{path}/17-18.csv"))
+temp_7 <- read.csv2(glue::glue("{path}/18-19.csv"))
+temp_8 <- read.csv2(glue::glue("{path}/19-20.csv"))
+temp_9 <- read.csv2(glue::glue("{path}/20-21.csv"))
 
 
+# 4 - Modificaciones a Tablas ---------------------------------------------
 
 temp_1 <- temp_1 %>% 
   select("Rk", "Squad", "GD") %>% 
@@ -79,31 +80,42 @@ temp_9 <- temp_9 %>%
 
 # 5 - Crear Ranking Dif Gol -----------------------------------------------
 
-temp_1$rank <- NA
-temp_1$rank[order(-temp_1$GD)] <- 1:nrow(temp_1)
+temp_1$RkGD <- NA
+temp_1$RkGD[order(-temp_1$GD)] <- 1:nrow(temp_1)
   
-temp_2$rank <- NA
-temp_2$rank[order(-temp_2$GD)] <- 1:nrow(temp_2)
+temp_2$RkGD <- NA
+temp_2$RkGD[order(-temp_2$GD)] <- 1:nrow(temp_2)
 
-temp_3$rank <- NA
-temp_3$rank[order(-temp_3$GD)] <- 1:nrow(temp_3)
+temp_3$RkGD <- NA
+temp_3$RkGD[order(-temp_3$GD)] <- 1:nrow(temp_3)
 
-temp_4$rank <- NA
-temp_4$rank[order(-temp_4$GD)] <- 1:nrow(temp_4)
+temp_4$RkGD <- NA
+temp_4$RkGD[order(-temp_4$GD)] <- 1:nrow(temp_4)
 
-temp_5$rank <- NA
-temp_5$rank[order(-temp_5$GD)] <- 1:nrow(temp_5)
+temp_5$RkGD <- NA
+temp_5$RkGD[order(-temp_5$GD)] <- 1:nrow(temp_5)
 
-temp_6$rank <- NA
-temp_6$rank[order(-temp_6$GD)] <- 1:nrow(temp_6)
+temp_6$RkGD <- NA
+temp_6$RkGD[order(-temp_6$GD)] <- 1:nrow(temp_6)
 
-temp_7$rank <- NA
-temp_7$rank[order(-temp_7$GD)] <- 1:nrow(temp_7)
+temp_7$RkGD <- NA
+temp_7$RkGD[order(-temp_7$GD)] <- 1:nrow(temp_7)
 
-temp_8$rank <- NA
-temp_8$rank[order(-temp_8$GD)] <- 1:nrow(temp_8)
+temp_8$RkGD <- NA
+temp_8$RkGD[order(-temp_8$GD)] <- 1:nrow(temp_8)
 
-temp_9$rank <- NA
-temp_9$rank[order(-temp_9$GD)] <- 1:nrow(temp_9)
+temp_9$RkGD <- NA
+temp_9$RkGD[order(-temp_9$GD)] <- 1:nrow(temp_9)
+
+# 6 - Diferencia Entre Temporadas -----------------------------------------
+
+dif_1 <- temp_1$RkGD - temp_2$Rk
+dif_2 <- temp_2$RkGD - temp_3$Rk
+dif_3 <- temp_3$RkGD - temp_4$Rk
+dif_4 <- temp_4$RkGD - temp_5$Rk
+dif_5 <- temp_5$RkGD - temp_6$Rk
+dif_6 <- temp_6$RkGD - temp_7$Rk
+dif_7 <- temp_7$RkGD - temp_8$Rk
+dif_8 <- temp_8$RkGD - temp_9$Rk
 
 
