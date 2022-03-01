7489f5fec1a63fb4edb69381d94f6da1c86ebd78
tbuscaglia <tebuscaglia@gmail.com>
2022-03-01 16:32:59 -0300

    Prueba de funcioes de worldfootballR

--- Scripts/PruebaTraerData.R
+++ Scripts/PruebaTraerData.R
@@ -6,7 +6,7 @@
 # 2 - Setup ---------------------------------------------------------------
 
 paquetes <- list(
-  "Tidyverse"     = list("dplyr"),
+  "Tidyverse"     = list("dplyr", "tidyr"),
   "Base de Datos" = list("worldfootballR"),
   "Web Scrapping" = list("rvest")
 )
@@ -20,7 +20,6 @@ lapply(
 
 rm(list = c("paquetes"))
 
-
 # 3 - Funciones worldfootballR --------------------------------------------
 
 partido <- "https://fbref.com/en/matches/4e263347/West-Ham-United-Newcastle-United-February-19-2022-Premier-League"
@@ -32,29 +31,56 @@ passing <- as_tibble(get_advanced_match_stats(
   stat_type      = "passing",
   team_or_player = "player"
   ) 
-) 
+) %>%
+  select(-c(
+    starts_with(c("Home", "Away")),
+    League, Gender, Country, Season, 
+    Match_Date, Matchweek, Game_URL,
+    Player_Num, Nation, Age
+    )
+  )
 
 possession <- get_advanced_match_stats(
   match_url      = partido, 
   stat_type      = "possession",
   team_or_player = "player"
-) 
+) %>% 
+  select(-c(
+    starts_with(c("Home", "Away")), 
+    League, Gender, Country, Season, 
+    Match_Date, Matchweek, Game_URL,
+    Player_Num, Nation, Age
+  )
+)
 
 defense <- get_advanced_match_stats(
   match_url      = partido, 
   stat_type      = "defense",
   team_or_player = "player"
+)%>% 
+  select(-c(
+    starts_with(c("Home", "Away")), 
+    League, Gender, Country, Season, 
+    Match_Date, Matchweek, Game_URL,
+    Player_Num, Nation, Age
+  )
 )
 
-# stats <- left_join(
-#   passing,
-#   possession,
-#   by = "Player"
-# ) %>% 
-#   left_join(
-#     .,
-#     defense,
-#     by = "Player"
-#   )
+stats <- left_join(
+   passing,
+   possession,
+   by = "Player"
+ ) %>% 
+   left_join(
+     .,
+     defense,
+     by = "Player"
+   ) 
+  
+
+# stats <- transpose(stats)
+# 
+# rownames(stats) <- colnames(stats)
+# colnames(stats) <- stats$Player
 
 
