505ae9ae97b98a08326461082240805ba4c44631
tbuscaglia <tebuscaglia@gmail.com>
2022-03-01 16:32:22 -0300

    Prueba de funciones de worldfootballR

--- Scripts/PruebaTraerData.R
+++ Scripts/PruebaTraerData.R
@@ -20,52 +20,41 @@ lapply(
 
 rm(list = c("paquetes"))
 
-# 3 - Pruebas de funciones ------------------------------------------------
 
+# 3 - Funciones worldfootballR --------------------------------------------
 
 partido <- "https://fbref.com/en/matches/4e263347/West-Ham-United-Newcastle-United-February-19-2022-Premier-League"
 
-
 # *1 - get_advanced_match_stats -------------------------------------------
 
-advanced_match_stats_passing <- as_tibble(get_advanced_match_stats(
+passing <- as_tibble(get_advanced_match_stats(
   match_url      = partido, 
   stat_type      = "passing",
   team_or_player = "player"
   ) 
-) %>% 
-  View()
+) 
 
-advanced_match_stats_shooting <- as_tibble(get_advanced_match_stats(
+possession <- get_advanced_match_stats(
   match_url      = partido, 
-  stat_type      = "possesion",
+  stat_type      = "possession",
   team_or_player = "player"
 ) 
-) %>% 
-  View()
 
-# *2 - get_match_report ---------------------------------------------------
+defense <- get_advanced_match_stats(
+  match_url      = partido, 
+  stat_type      = "defense",
+  team_or_player = "player"
+)
 
-get_match_report(match_url = partido) %>% View()
+# stats <- left_join(
+#   passing,
+#   possession,
+#   by = "Player"
+# ) %>% 
+#   left_join(
+#     .,
+#     defense,
+#     by = "Player"
+#   )
 
-worldfootballR::get_match_shooting(match_url = partido) %>% as_tibble() %>% View()
 
-# *3 - get_match_shooting/summary -----------------------------------------
-
-get_match_shooting(partido) %>% View()
-
-get_match_summary(partido) %>% View()
-
-# 4 - Web Scrapping -------------------------------------------------------
-
-link <- "https://fbref.com/en/matches/4e263347/West-Ham-United-Newcastle-United-February-19-2022-Premier-League"
-conexion <- "#stats_7c21e445_summary .right , #stats_7c21e445_summary , #stats_7c21e445_summary .center, #stats_7c21e445_summary .left"
-
-pagina <- read_html(link)
-
-summary_stats <- pagina %>% 
-  html_nodes(conexion) %>% 
-  html_text()
-   
-summary_stats <- as_tibble(summary_stats)
-  
