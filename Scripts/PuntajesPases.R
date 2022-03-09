4625c2d1962f0060ffdb2b5ce9d76faf4028c1c3
tbuscaglia <tebuscaglia@gmail.com>
2022-03-08 22:29:01 -0300

    Se comenzo a escribir codigo para crear puntajes

---
+++ Scripts/PuntajesPases.R
@@ -0,0 +1,71 @@
+
+# 1 - Objetivo ------------------------------------------------------------
+
+# Crear un puntaje de jugadores por partido en base a estadisticas del partido
+
+# 2 - Setup ---------------------------------------------------------------
+
+paquetes <- list(
+  "Tidyverse"     = list("dplyr", "tidyr"),
+  "Base de Datos" = list("worldfootballR")
+)
+
+lapply(
+  as.list(
+    c(paquetes, recursive = TRUE, use.names = FALSE)),
+  function(x) {
+    library(x, character.only = TRUE, verbose = FALSE)
+  })
+
+rm(list = c("paquetes"))
+
+# 3- Conexion a API -------------------------------------------------------
+
+# Tottenham Hotspur vs. Everton – Monday March 7, 2022
+
+partido <- "https://fbref.com/en/matches/6e11eac6/Tottenham-Hotspur-Everton-March-7-2022-Premier-League"
+
+# 4 - Busco Estadisticas --------------------------------------------------
+
+pases <- get_advanced_match_stats(
+  match_url      = partido,
+  stat_type      = "passing",
+  team_or_player = "player"
+) %>% 
+  select(Team, Player, Pos, Min, Cmp_Total, Att_Total, Cmp_percent_Total) %>% 
+  mutate(
+    CmpPerMin            = Cmp_Total / Min,
+    AttPerMin            = Att_Total / Min,
+    Mss_percent_Total    = 100 - Cmp_percent_Total,
+    Mss_Total            = Cmp_Total - Att_Total,
+    # Cmp_percent_Quantile = quantile(Cmp_percent_Total, seq(0, 1, nrow(pases))),
+    across(c(CmpPerMin, AttPerMin), ~ round(., digits = 3))
+  )
+
+# 5 - Crear Puntaje -------------------------------------------------------
+
+pases <- pases %>% 
+  mutate(
+    Pts_Inicial     = 6,
+    Pts_Min         = Min * 0.01,
+    Pts_Cmp_Total   = Cmp_Total * 0.05,
+    Pts_Err_Total   = Mss_Total * 0.05,
+    Pts_Cmp_percent = if (Cmp_percent_Quantile < 25) {
+      -2
+    } else if (between(Cmp_percent_Quantile, 20, 50)) {
+      -1
+    } else if (between(Cmp_percent_Quantile, 50, 60)) {
+      1
+    } else if (between(Cmp_percent_Quantile, 60, 70)) {
+      1.5
+    } else if (between(Cmp_percent_Quantile, 70, 80)) {
+      2
+    } else if (between(Cmp_percent_Quantile, 80, 90)) {
+      2.5
+    } else if (between(Cmp_percent_Quantile, 90, 100)) {
+      3
+    }
+  )
+
+
+
